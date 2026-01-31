42601e206313bf380f344ba3660c533cb1136668
Zoez0802 <minjiez@bu.edu>
2026-01-31 18:18:26 -0500

    Set restaurant as root homepage

--- cs412/urls.py
+++ cs412/urls.py
@@ -21,5 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path("hw/", include ("hw.urls")),
     path("formdata/", include ("formdata.urls")),
-    path("restaurant/", include ("restaurant.urls")),#NEW
+    path("", include ("restaurant.urls")),#NEW
 ]
