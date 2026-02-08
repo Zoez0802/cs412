60b77bc25017bb2c60cea85bcea408358127fab9
Zoez0802 <minjiez@bu.edu>
2026-02-08 15:29:35 -0500

    basic implementation of Assignment 3

--- mini_insta/views.py
+++ mini_insta/views.py
@@ -1,4 +1,4 @@
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import Profile
 
 class ProfileListView(ListView):
@@ -6,3 +6,8 @@ class ProfileListView(ListView):
     model = Profile
     template_name = "mini_insta/show_all_profiles.html"
     context_object_name = "profiles"
+
+class ProfileDetailView(DetailView):
+    model = Profile
+    template_name = "mini_insta/show_profile.html"
+    context_object_name = "profile"
